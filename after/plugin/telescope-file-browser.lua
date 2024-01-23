e820a697f062d97831fc41eb4c93c4ecb035aac5
Luca Carella <c30ra@users.noreply.github.com>
2024-01-23 23:54:00 +0100

    feattelescope-file-browser: added telescope-file-browser

---
+++ after/plugin/telescope-file-browser.lua
@@ -0,0 +1,30 @@
+require("telescope").setup {
+  extensions = {
+    file_browser = {
+      theme = "ivy",
+      -- disables netrw and use telescope-file-browser in its place
+      hijack_netrw = true,
+      -- mappings = {
+      --   ["i"] = {
+      --     -- your custom insert mode mappings
+      --   },
+      --   ["n"] = {
+      --     -- your custom normal mode mappings
+      --   },
+      -- },
+    },
+  },
+}
+
+require('which-key').register {
+  ['<leader>f'] = { name = '[F]ile', _ = 'which_key_ignore' },
+}
+vim.api.nvim_set_keymap(
+  "n",
+  "<space>fb",
+  ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+  { noremap = true , desc="Browser"}
+)
+-- To get telescope-file-browser loaded and working with telescope,
+-- you need to call load_extension, somewhere after setup function:
+require("telescope").load_extension "file_browser"
