5d5d9ab935d9789c5cba2c8cbf6474482df37e32
Luca Carella <c30ra@users.noreply.github.com>
2024-03-01 01:02:24 +0100

    added codeium

---
+++ after/plugin/codeium.lua
@@ -0,0 +1,6 @@
+vim.keymap.set("n", "<space>cc", function() return vim.fn['codeium#Chat']() end,
+  { noremap = true, desc = "Codeium Chat" })
+
+-- write a bind that doesn't conflict with nvim-cmp when accepting a completition
+vim.keymap.set("i", "<TAB>", function() return vim.fn['codeium#Accept']() end,
+  { noremap = true, desc = "Codeium Accept" })
