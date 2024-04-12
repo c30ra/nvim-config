ea12a599a2411aeabfbbbee98384dc30b4c54dd9
Luca Carella <c30ra@users.noreply.github.com>
2024-04-12 21:03:22 +0200

    Update autoformatter.lua
    added more milliseconds to timeout

--- after/plugin/autoformatter.lua
+++ after/plugin/autoformatter.lua
@@ -8,7 +8,7 @@ require("conform").setup({
 	},
 	format_on_save = {
 		-- These options will be passed to conform.format()
-		timeout_ms = 500,
+		timeout_ms = 5000,
 		lsp_fallback = true,
 	},
 })
