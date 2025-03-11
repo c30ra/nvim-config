50ec25b177dc4ecbc455843e1ea47004b1469dd7
Luca Carella <c30ra@users.noreply.github.com>
2025-03-11 23:58:36 +0100

    added inline dap

--- lua/config/plugins/dap.lua
+++ lua/config/plugins/dap.lua
@@ -1,19 +1,20 @@
 return {
-  -- NOTE: Yes, you can install new plugins here!
-  'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
-  dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
-	'nvim-neotest/nvim-nio',
+	-- NOTE: Yes, you can install new plugins here!
+	'mfussenegger/nvim-dap',
+	-- NOTE: And you can specify dependencies as well
+	dependencies = {
+		-- Creates a beautiful debugger UI
+		'rcarriga/nvim-dap-ui',
+		'nvim-neotest/nvim-nio',
 
-    -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
+		-- Installs the debug adapters for you
+		'williamboman/mason.nvim',
+		'jay-babu/mason-nvim-dap.nvim',
 
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
-    'mfussenegger/nvim-dap-python'
-  },
-    lazy=true,
+		-- Add your own debuggers here
+		'leoluz/nvim-dap-go',
+		'mfussenegger/nvim-dap-python',
+		'theHamsta/nvim-dap-virtual-text'
+	},
+	lazy = true,
 }
